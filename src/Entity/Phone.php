aa6709220c4bb6a66f0ca6d2a78bff2ac40e8bad
fra9106 <franckbouto@monpersoweb.fr>
2021-01-27 09:30:10 +0100

    delete controllers

--- src/Entity/Phone.php
+++ src/Entity/Phone.php
@@ -4,10 +4,14 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\PhoneRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=PhoneRepository::class)
+ * @ApiResource(
+ *     collectionOperations={}
+ * )
  */
 class Phone
 {
